ec415d1023c27ae1b1ba990a8b02114a66a1a1e9
unknown <singersman@naver.com>
2021-04-05 16:50:08 +0900

    Modify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React from "react";
 
-function Food() {
-  return <h1>I like potato</h1>;
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimch",
+    image:
+      "https://cdn.crowdpic.net/detail-thumb/thumb_d_CDC14868821FF3F20C77BC8BC15E6355.jpg",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Kimbab",
+    image:
+      "https://www.gyeongju.go.kr/upload/content/thumb/20200506/9DD6ED74C34C4B0B9FA0BA67F49C3BEE.jpg",
+    rating: 7,
+  },
+  {
+    id: 3,
+    name: "Samgiopsal",
+    image:
+      "http://ojsfile.ohmynews.com/STD_IMG_FILE/2017/0719/IE002190922_STD.JPG",
+    rating: 10,
+  },
+];
+
+Food.propTypes = {};
+
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>Rating {rating} / 10.0 </h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
-      <Food name="kimchi" />
+      {foodILike.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
